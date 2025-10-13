54ff03c0cb2a8a12b92ae65f39b5f8875cbcda45
ArielWT <Ariel.ywt@gmail.com>
2025-10-12 21:46:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-// server.js
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Carpeta con tu webapp estática
+// Si tus archivos están en webapp/
 const staticDir = path.join(__dirname, 'webapp');
-app.use(express.static(staticDir));
+// Si todavía están en la raíz, usa: const staticDir = __dirname;
 
-// Si quieres soportar rutas tipo SPA, descomenta esto:
+app.use(express.static(staticDir));
+// Si en el futuro usas SPA:
 // app.get('*', (_, res) => res.sendFile(path.join(staticDir, 'index.html')));
 
-app.listen(PORT, () => {
-  console.log(`Servidor web escuchando en puerto ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor web escuchando en puerto ${PORT}`));
